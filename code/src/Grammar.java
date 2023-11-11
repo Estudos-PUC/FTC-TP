4a7f7278041ca1b7297f513227134316928bc30a
Pedro Olyntho <66219757+pedroolynth0@users.noreply.github.com>
2023-11-11 10:00:50 -0300

    Grammar

---
+++ code/src/Grammar.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * Classe utilizada para transformar os dados dos txt em uma GLC
+ */
+public class Grammar {
+    private Map<String, List<String>> grammar = new HashMap<>();
+
+    public List<String> getKey(String key) {
+        return grammar.get(key);
+    }
+
+    public void loadGrammar(String filePath){
+        try {
+            File file = new File(filePath);
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String[] parts = sc.nextLine().split(" ");
+
+                String key = parts[0];
+                List<String> value = Arrays.asList(parts).subList(1, parts.length);
+                grammar.put(key, value);
+            }
+            sc.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
--- code/src/Main.java
+++ code/src/Main.java
@@ -1,22 +1,9 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-// import java.io.FileWriter;
-// import java.io.IOException;
-// import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        try {
-            
-            String arquivo = "code/model/GLC.txt";
-            File file = new File(arquivo);
-            Scanner sc = new Scanner(file);
-            System.out.println(sc.nextLine());
-
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-
+        Grammar grammar = new Grammar();
+        grammar.loadGrammar("code/model/GLC.txt");
+        System.out.println(grammar.getKey("T"));
     }
 }
