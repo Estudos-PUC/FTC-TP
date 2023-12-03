74bb130e359818fd5af9fad66a2b2cd6e626160f
Pedro Olyntho <66219757+pedroolynth0@users.noreply.github.com>
2023-12-02 23:55:06 -0300

    FEITOOOOOOOO

---
+++ code/model/gerador.py
@@ -0,0 +1,48 @@
+import itertools
+
+# Regras da gramática
+rules = {
+    "S": ["AED", "F", "SI", "HS"],
+    "A": ["Aa", "a", "AB"],
+    "B": ["Bb", "b", "BA"],
+    "C": ["Cc", "c", "CI"],
+    "D": ["Dd", "d", "DC"],
+    "E": ["bEc", "bc", "EA"],
+    "F": ["aFd", "BC", "FB"],
+    "H": ["abbbbc", "HA"],
+    "I": ["Id", "IA", "IH"]
+}
+
+# Terminais fornecidos
+terminals = ["a", "b", "c", "d"]
+
+# Função para gerar combinações de terminais
+def generate_combinations(symbol, depth, max_depth):
+    if depth > max_depth:
+        return []
+
+    combinations = []
+    if symbol in terminals:
+        return [symbol]
+    elif symbol in rules:
+        for production in rules[symbol]:
+            sub_combinations = [generate_combinations(sub_symbol, depth + 1, max_depth) 
+                                for sub_symbol in production]
+            product = ["".join(combo) for combo in itertools.product(*sub_combinations)]
+            combinations.extend(product)
+    return combinations
+
+# Gerar combinações
+max_depth = 5  # Limitar a profundidade para evitar recursão infinita
+start_symbol = "S"  # Símbolo inicial
+
+# Gerar combinações a partir do símbolo inicial
+terminal_combinations = generate_combinations(start_symbol, 0, max_depth)
+
+# Filtrar combinações únicas e não vazias
+unique_combinations = set(filter(None, terminal_combinations))
+
+# Exibir as combinações
+for combo in unique_combinations:
+    print(combo)
+
